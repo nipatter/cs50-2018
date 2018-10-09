8e700e8d45a6c46184fc2b568480433f3bfe38b7
Nigel Patterson <nigel.pattterson@gmail.com>
2018-10-09 02:03:25 +0000

    cmdline argv validation to caesar + vigenere, but vigenere still buggy

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -4,27 +4,31 @@
  * corresponding letter of the keytext.
  */
 
+// TODO: this doesnt catch whitespace in command line argument
+
 #include <stdio.h>
 #include <cs50.h>       // easy user input
 #include <ctype.h>      // checking character types
-#include <stdlib.h>     // atoi() function
+#include <stdlib.h>     // atoi() and exit() functions
 #include <string.h>     // strlen() function
 
 
 int main(int argc, char *argv[])
 {
     // checking that argv[1] is only text
-    int c;
     int argv_len = strlen(argv[1]);
-    for (c = 0; c < argv_len; c++)
+    for (int c = 0; c < argv_len; c++)
     {
         if (isalpha(argv[1][c]) == 0)
-            printf("not ok\n");
+        {
+            printf("USAGE: ./vigenere ABC (where k is any length alphabet string)\n");
+            exit(0);
+        }
     }
 
     // turning argv[1] into a numeric key
     int keys[argv_len];
-    for (c = 0; c < argv_len; c++)
+    for (int c = 0; c < argv_len; c++)
     {
         keys[c] = toupper(argv[1][c]) - 65;
     }
@@ -34,27 +38,26 @@ int main(int argc, char *argv[])
     int text_len = strlen(plain_text);
 
     // Vigenere encryption
-    int i, j = 0;
-    char cipher_text[text_len];
-    for (i = 0; i < text_len; i++)
+    int j = 0;
+    for (int i = 0; i < text_len; i++)
     {
         if (isalpha(plain_text[i]) && isupper(plain_text[i]))
         {
-            cipher_text[i] = ((plain_text[i] + keys[j] - 65) % 26) + 65;
+            printf("%c\n", ((plain_text[i] + keys[j] - 65) % 26) + 65);
             j = (j + 1) % argv_len;
         }
         else if (isalpha(plain_text[i]) && islower(plain_text[i]))
         {
-            cipher_text[i] = ((plain_text[i] + keys[j] - 97) % 26) + 97;
+            printf("%c\n", ((plain_text[i] + keys[j] - 97) % 26) + 97);
             j = (j + 1) % argv_len;
         }
         else
         {
-            cipher_text[i] = plain_text[i];
+            printf("%c\n", plain_text[i]);
         }
     }
 
-    // print the cipher text
-    printf("%s\n", cipher_text);
+    // print the cypher text
+    printf("\n");
     return 0;
 }
